a42dd29203cd8357011ab68286f06441d26d1a33
Pood0 <gerdemanrb@appstate.edu>
2023-11-22 14:55:32 -0500

    working on basic structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'playmaker', component: PlaymakerComponent },
+  { path: 'about', component: AboutComponent },
+  // other routes...
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
